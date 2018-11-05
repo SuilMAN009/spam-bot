8e51a3baf24f092e54c2d5abaa38861fafd61610
SuilMAN009 <44708799+SuilMAN009@users.noreply.github.com>
2018-11-05 23:14:53 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By The_Ghast");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("509072641513226245") 
+setInterval(function() {
+channel.send(`IixRp_ 7lw`)
+channel.send(`fdet IixRp_`)
+channel.send(`<3 IixRp_`)
+channel.send(`a7bk IixRp_`)
+channel.send(`a7oobkkk`)
+}, 25)
+})
+ 
+ 
+client.login('NDUzOTkzMzYwMzY1OTc3NjIx.DsIm7w.tEQBuwGPJg5YHb8iFqw-DaunFMA');
